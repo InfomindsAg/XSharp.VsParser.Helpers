3e8a2fa18d300ffa5b7ce6e251b676314eee7df6
Volkmar Rigo <volkmar.rigo@infominds.eu>
2021-07-25 17:41:12 +0200

    Added Dump as XML and Yaml

--- src/XSharp.VsParser.Helpers/Parser/ParserHelper.cs
+++ src/XSharp.VsParser.Helpers/Parser/ParserHelper.cs
@@ -7,12 +7,15 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
+using System.Xml.Linq;
 using XSharp.Parser.Helpers.Parser;
 
 namespace XSharp.Parser.Helpers
 {
     public class ParserHelper
     {
+        const string NewList = "$NewLine";
         GenericErrorListener _ErrorListener = new();
         string _SourceCode;
         XSharpParseOptions _XSharpOptions;
@@ -21,6 +24,12 @@ namespace XSharp.Parser.Helpers
         protected XSharpParserRuleContext _StartRule;
         protected bool _ParseSuccessful;
         protected string _FileName;
+
+        void CheckParseSuccessful()
+        {
+            if (!_ParseSuccessful)
+                throw new ArgumentException("Parsing was not successful");
+        }
 
         internal ParserHelper(XSharpParseOptions xsharpOptions)
         {
@@ -65,8 +74,7 @@ namespace XSharp.Parser.Helpers
         public void ExecuteListeners(List<XSharpBaseListener> listeners)
         {
             Debug.Assert((listeners?.Count ?? 0) > 0, "List of listeners can not be empty");
-            if (!_ParseSuccessful)
-                throw new ArgumentException("Parsing was not successful");
+            CheckParseSuccessful();
 
             var walker = new ParseTreeWalker();
             foreach (var listener in listeners)
@@ -76,8 +84,7 @@ namespace XSharp.Parser.Helpers
         public (bool changed, string newSourceCode) ExecuteRewriters(List<XSharpBaseRewriter> rewriters)
         {
             Debug.Assert((rewriters?.Count ?? 0) > 0, "List of rewriters can not be empty");
-            if (!_ParseSuccessful)
-                throw new ArgumentException("Parsing was not successful");
+            CheckParseSuccessful();
 
             var tokenRewriter = new TokenStreamRewriter(_Tokens);
             var walker = new ParseTreeWalker();
@@ -104,6 +111,91 @@ namespace XSharp.Parser.Helpers
             return changed;
         }
 
+
+        /// <summary>
+        /// Dumps the AST created by parsing as XDocument
+        /// </summary>
+        public XDocument DumpXml()
+        {
+            CheckParseSuccessful();
+
+            static void DumpTerminalValue(TerminalNodeImpl terminalNodeImpl, XElement element)
+            {
+                var text = terminalNodeImpl.Payload.Text;
+                if (text == Environment.NewLine)
+                    element.Add(NewList);
+                else if (text.IndexOfAny(new char[] { '\n', '<', '>', '&' }) == -1)
+                    element.Add(text);
+                else
+                    element.Add(new XCData(text));
+            }
+
+            static void DumpElement(IParseTree rule, XContainer container)
+            {
+                var node = new XElement(rule.GetType().Name);
+                container.Add(node);
+
+                if (rule is TerminalNodeImpl terminalNodeImpl && rule.ChildCount == 0)
+                    DumpTerminalValue(terminalNodeImpl, node);
+
+                for (int i = 0; i < rule.ChildCount; i++)
+                    DumpElement(rule.GetChild(i), node);
+            }
+
+            var doc = new XDocument();
+            DumpElement(_StartRule, doc);
+            return doc;
+        }
+
+        /// <summary>
+        /// Dumps the AST created by parsing as Yaml
+        /// </summary>
+        public string DumpYaml()
+        {
+            CheckParseSuccessful();
+            var sb = new StringBuilder();
+
+            void DumpTerminalValue(string text, int indent)
+            {
+                if (text == Environment.NewLine)
+                {
+                    sb.AppendLine(" " + NewList);
+                    return;
+                }
+
+                text = text.Replace("\\", "\\\\").Replace("\"", "\\\"");
+                if (text.Contains("\n"))
+                {
+                    sb.AppendLine(" |-");
+                    foreach (var line in text.Replace("\r", "").Split('\n'))
+                        sb.AppendLine(new string(' ', indent * 2) + line);
+                }
+                else
+                    sb.Append(" \"").Append(text).AppendLine("\"");
+            }
+
+            void DumpElement(IParseTree rule, int indent)
+            {
+                var indentString = new string(' ', indent * 2);
+                indent++;
+
+                sb.Append(indentString).Append("- ").Append(rule.GetType().Name).Append(":");
+
+                if (rule is TerminalNodeImpl terminalNodeImpl && rule.ChildCount == 0)
+                    DumpTerminalValue(terminalNodeImpl.Payload.Text, indent + 1);
+                else
+                {
+                    sb.AppendLine();
+                    for (int i = 0; i < rule.ChildCount; i++)
+                        DumpElement(rule.GetChild(i), indent);
+                }
+            }
+
+            sb.Append(_StartRule.GetType().Name).AppendLine(":");
+            for (int i = 0; i < _StartRule.ChildCount; i++)
+                DumpElement(_StartRule.GetChild(i), 0);
+            return sb.ToString();
+        }
 
         #region Builders
 
