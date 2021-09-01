f61f1979268332375f63ede6fc6128352a6f996e
Volkmar Rigo <volkmar.rigo@infominds.eu>
2021-09-01 19:34:48 +0200

    RewriterForConstructor

---
+++ src/XSharp.VsParser.Helpers.Tests/Rewriter/ConstructorContextTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using XSharp.Parser.Helpers.Tests.TestHelpers;
+using XSharp.VsParser.Helpers.Parser;
+using XSharp.VsParser.Helpers.Rewriter;
+using Xunit;
+using static LanguageService.CodeAnalysis.XSharp.SyntaxParser.XSharpParser;
+
+namespace XSharp.Parser.Helpers.Tests.Rewriter
+{
+    public class ConstructorContextTests : TestsFor<ConstructorContext>
+    {
+
+        [Fact]
+        public void ReplaceCallingConventionTest()
+        {
+            var code = WrapInClass(@"Constructor () strict
+return nil");
+
+            var expected = WrapInClass(@"Constructor () clipper
+return nil");
+
+            Rewrite(code, expected, r => r.ReplaceCallingConvention("clipper"));
+        }
+
+        [Fact]
+        public void DeleteCallingConventionTest()
+        {
+            var code = WrapInClass(@"Constructor () strict
+return nil");
+
+            var expected = WrapInClass(@"Constructor ()
+return nil");
+
+            Rewrite(code, expected, r => r.DeleteCallingConvention());
+        }
+
+        [Fact]
+        public void AddParameterEmptyTest()
+        {
+            var code = WrapInClass(@"Constructor ()
+return nil");
+
+            var expected = WrapInClass(@"Constructor (newParam)
+return nil");
+
+            Rewrite(code, expected, r => r.AddParameter("newParam"));
+        }
+
+        [Fact]
+        public void AddParameterTypedEmptyTest()
+        {
+            var code = WrapInClass(@"Constructor ()
+return nil");
+
+            var expected = WrapInClass(@"Constructor (newParam as string)
+return nil");
+
+            Rewrite(code, expected, r => r.AddParameter("newParam as string"));
+        }
+
+        [Fact]
+        public void AddParameterNotEmptyTest()
+        {
+            var code = WrapInClass(@"Constructor (firstParam as string)
+return nil");
+
+            var expected = WrapInClass(@"Constructor (firstParam as string, newParam as string)
+return nil");
+
+            Rewrite(code, expected, r => r.AddParameter("newParam as string"));
+        }
+
+    }
+}
---
+++ src/XSharp.VsParser.Helpers/Rewriter/RewriterForConstructorExtensions.cs
@@ -0,0 +1,65 @@
+﻿using LanguageService.SyntaxTree.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using XSharp.VsParser.Helpers.Parser;
+using static LanguageService.CodeAnalysis.XSharp.SyntaxParser.XSharpParser;
+
+namespace XSharp.VsParser.Helpers.Rewriter
+{
+    /// <summary>
+    /// RewriterForIdentifier Extensions
+    /// </summary>
+    public static class RewriterForConstructorExtensions
+    {
+        static RewriterForContext<ParameterListContext> RewriterFoParameterList(RewriterForContext<ConstructorContext> rewriterFor)
+                    => rewriterFor.RewriterFor(rewriterFor.Context?.ParamList);
+
+        /// <summary>
+        /// Deletes all the parameters of the construcor
+        /// </summary>
+        /// <param name="rewriterFor">The rewriterFor instance</param>
+        /// <returns>The rewriterFor instance</returns>
+        public static RewriterForContext<ConstructorContext> DeleteAllParameters(this RewriterForContext<ConstructorContext> rewriterFor)
+        {
+            RewriterFoParameterList(rewriterFor).DeleteAllParameters();
+            return rewriterFor;
+        }
+
+        /// <summary>
+        /// Adds a new parameters at the end of the parameterlist
+        /// </summary>
+        /// <param name="rewriterFor">The rewriterFor instance</param>
+        /// <param name="newParameter">The new parameter</param>
+        /// <returns>The rewriterFor instance</returns>
+        public static RewriterForContext<ConstructorContext> AddParameter(this RewriterForContext<ConstructorContext> rewriterFor, string newParameter)
+        {
+            RewriterFoParameterList(rewriterFor).AddParameter(newParameter);
+            return rewriterFor;
+        }
+
+        /// <summary>
+        /// Replaces the calling convention of the constructor 
+        /// </summary>
+        /// <param name="rewriterFor">The rewriterFor instance</param>
+        /// <param name="newCallingConvention">The new calling convention</param>
+        /// <returns>The rewriterFor instance</returns>
+        public static RewriterForContext<ConstructorContext> ReplaceCallingConvention(this RewriterForContext<ConstructorContext> rewriterFor, string newCallingConvention)
+        {
+            InternalRewriterHelper.ReplaceCallingConvention(rewriterFor.Rewriter, newCallingConvention, rewriterFor.Context.callingconvention(), rewriterFor.Context.Stop);
+            return rewriterFor;
+        }
+
+        /// <summary>
+        /// Deletes the calling convention
+        /// </summary>
+        /// <param name="rewriterFor">The rewriterFor instance</param>
+        /// <returns>The rewriterFor instance</returns>
+        public static RewriterForContext<ConstructorContext> DeleteCallingConvention(this RewriterForContext<ConstructorContext> rewriterFor)
+        {
+            InternalRewriterHelper.DeleteCallingConvention(rewriterFor.Rewriter, rewriterFor.Context.callingconvention());
+            return rewriterFor;
+        }
+
+    }
+}
