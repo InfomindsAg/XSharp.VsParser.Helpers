a6472301963c5d5812d43c5fbec7ddac99859e43
Volkmar Rigo <volkmar.rigo@infominds.eu>
2021-09-16 11:49:49 +0200

    Add Localvar Rewriter

---
+++ src/XSharp.VsParser.Helpers.Tests/Rewriter/LocalvarContextTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using XSharp.Parser.Helpers.Tests.TestHelpers;
+using XSharp.VsParser.Helpers.Parser;
+using XSharp.VsParser.Helpers.Rewriter;
+using Xunit;
+using static LanguageService.CodeAnalysis.XSharp.SyntaxParser.XSharpParser;
+
+namespace XSharp.Parser.Helpers.Tests.Rewriter
+{
+    public class LocalvarContextTests : TestsFor<LocalvarContext>
+    {
+        [Fact]
+        public void ReplaceTypeEmptyTest()
+        {
+            var code = WrapInMethod(@"local a");
+
+            var expected = WrapInMethod(@"local a as string");
+
+            Rewrite(code, expected, r => r.ReplaceDataType("string"));
+        }
+
+        [Fact]
+        public void ReplaceTypeDifferentTest()
+        {
+            var code = WrapInMethod(@"local a as int");
+
+            var expected = WrapInMethod(@"local a as string");
+
+            Rewrite(code, expected, r => r.ReplaceDataType("string"));
+        }
+
+        [Fact]
+        public void ReplaceTypeMultipleEmptyTest()
+        {
+            var code = WrapInMethod(@"local a, b");
+
+            var expected = WrapInMethod(@"local a as string, b as string");
+
+            Rewrite(code, expected, r => r.ReplaceDataType("string"));
+        }
+
+        [Fact]
+        public void ReplaceTypeMultipleEmptyWithInitTest()
+        {
+            var code = WrapInMethod(@"local a := """", b := """"");
+
+            var expected = WrapInMethod(@"local a := """" as string, b := """" as string");
+
+            Rewrite(code, expected, r => r.ReplaceDataType("string"));
+        }
+
+
+        
+
+    }
+}
---
+++ src/XSharp.VsParser.Helpers/Rewriter/RewriterForLocalvar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using XSharp.VsParser.Helpers.Parser;
+using static LanguageService.CodeAnalysis.XSharp.SyntaxParser.XSharpParser;
+
+namespace XSharp.VsParser.Helpers.Rewriter
+{
+    /// <summary>
+    /// RewriterForIdentifier Extensions
+    /// </summary>
+    public static class RewriterForLocalvar
+    {
+        /// <summary>
+        /// Replaces the data type of the local variables with a new data type
+        /// </summary>
+        /// <param name="rewriterFor">The rewriterFor instance</param>
+        /// <param name="newDatatype">the new data type</param>
+        /// <returns>The rewriterFor instance</returns>
+        public static RewriterForContext<LocalvarContext> ReplaceDataType(this RewriterForContext<LocalvarContext> rewriterFor, string newDatatype)
+        {
+            if (string.IsNullOrEmpty(newDatatype))
+                throw new ArgumentException("NewDataType can not be emtpy");
+
+            var localContext = rewriterFor.Context;
+            if (localContext.DataType != null)
+                rewriterFor.Rewriter.Replace(localContext.datatype().Start.ToIndex(), localContext.datatype().stop.ToIndex(), newDatatype);
+            else
+            {
+                var index = localContext.Id.stop.ToIndex();
+                if (localContext.Expression != null)
+                    index = localContext.Expression.Stop.ToIndex();
+                rewriterFor.Rewriter.InsertAfter(index, $" as {newDatatype}");
+            }
+
+            return rewriterFor;
+        }
+
+    }
+}
