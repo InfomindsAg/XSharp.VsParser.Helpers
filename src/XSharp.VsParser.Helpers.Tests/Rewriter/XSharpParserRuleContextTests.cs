76dfab47c9f5eeed6b0a71a8a11c9f898c95734b
Volkmar Rigo <volkmar.rigo@infominds.eu>
2021-11-22 19:07:08 +0100

    Rewriter for XSharpParserRule

---
+++ src/XSharp.VsParser.Helpers.Tests/Rewriter/XSharpParserRuleContextTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using LanguageService.CodeAnalysis.XSharp.SyntaxParser;
+using System;
+using System.Linq;
+using XSharp.Parser.Helpers.Tests.TestHelpers;
+using XSharp.VsParser.Helpers.Parser;
+using XSharp.VsParser.Helpers.Rewriter;
+using Xunit;
+using static LanguageService.CodeAnalysis.XSharp.SyntaxParser.XSharpParser;
+
+namespace XSharp.Parser.Helpers.Tests.Rewriter
+{
+    public class XSharpParserRuleContextTests : TestsFor<XSharpParserRuleContext>
+    {
+        [Fact]
+        public void ReplaceTest()
+        {
+            var code = WrapInMethod(@"self:Icon := oIcon");
+
+            var expected = WrapInMethod(@"self:Icon := (Icon)oIcon");
+
+            var parser = code.ParseText();
+            var assignment = parser.Tree.FirstOrDefaultType<AssignmentExpressionContext>();
+            parser.Tree.RewriterFor(assignment.Right).Replace("(Icon)oIcon");
+
+            parser.Tree.Rewriter.GetText().Should().Be(expected);
+        }
+    }
+}
---
+++ src/XSharp.VsParser.Helpers/Rewriter/RewriterForXSharpParserRuleExtensions.cs
@@ -0,0 +1,31 @@
+﻿using LanguageService.CodeAnalysis.XSharp.SyntaxParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using XSharp.VsParser.Helpers.Parser;
+using static LanguageService.CodeAnalysis.XSharp.SyntaxParser.XSharpParser;
+
+namespace XSharp.VsParser.Helpers.Rewriter
+{
+    /// <summary>
+    /// RewriterForXSharpParserRule Extensions
+    /// </summary>
+    public static class RewriterForXSharpParserRuleExtensions
+    {
+
+        /// <summary>
+        /// Replaces the token
+        /// </summary>
+        /// <param name="rewriterFor">The rewriterFor instance</param>
+        /// <param name="newContent">The new content</param>
+        /// <returns>The rewriterFor instance</returns>
+        public static RewriterForContext<T> Replace<T>(this RewriterForContext<T> rewriterFor, string newContent) where T : XSharpParserRuleContext
+        {
+            if (rewriterFor.Context == null)
+                throw new ArgumentException("Context can not be empty");
+
+            rewriterFor.Rewriter.Replace(rewriterFor.Context.start.ToIndex(), rewriterFor.Context.stop.ToIndex(), newContent);
+            return rewriterFor;
+        }
+    }
+}
