c45e2c29e6ea0f70fdbe4dff7f367bc127167750
Patrick Kruselburger <pkruselburger@gmail.com>
2022-07-28 17:10:46 +0200

    Add Classvar rewriter.ReplaceType

---
+++ src/XSharp.VsParser.Helpers.Tests/Rewriter/ClassVarContextTests.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using System.Linq;
+using XSharp.Parser.Helpers.Tests.TestHelpers;
+using XSharp.VsParser.Helpers.Parser;
+using XSharp.VsParser.Helpers.Parser.Values;
+using XSharp.VsParser.Helpers.Rewriter;
+using Xunit;
+using static LanguageService.CodeAnalysis.XSharp.SyntaxParser.XSharpParser;
+
+namespace XSharp.Parser.Helpers.Tests.Rewriter
+{
+    public class ClassVarContextTests : TestsFor<ClassvarContext>
+    {
+
+        [Fact]
+        public void ReplaceTypeClassVarTest()
+        {
+            var code = WrapInClass(@"protected a");
+
+            var expected = WrapInClass(@"protected a as string");
+
+            Rewrite(code, expected, r => r.ReplaceType("string"));
+        }
+
+        [Fact]
+        public void ReplaceTypeClassVarsTest()
+        {
+            var code = WrapInClass(@"protected a as int, b, c as usual");
+            var expected = WrapInClass(@"protected a as int, b as string, c as usual");
+            var parser = code.ParseText();
+            var vars = GetVars(parser);
+
+            var toBeReplacedVar = vars.Single(x => x.Name.Equals("b")).Context;
+
+            parser.Tree.RewriterFor(toBeReplacedVar).ReplaceType("string");
+            AssertRewriterTextEquals(parser, expected);
+        }
+
+        [Fact]
+        public void ReplaceTypeClassVarsTest2()
+        {
+            var code = WrapInClass(@"protected a as int, b, c as usual");
+            var expected = WrapInClass(@"protected a as int, b as usual, c as string");
+            var parser = code.ParseText();
+            var vars = GetVars(parser);
+
+            var toBeReplacedVar = vars.Single(x => x.Name.Equals("c")).Context;
+
+            parser.Tree.RewriterFor(toBeReplacedVar).ReplaceType("string");
+            AssertRewriterTextEquals(parser, expected);
+        }
+
+        [Fact]
+        public void ReplaceTypeClassVarsTest3()
+        {
+            var code = WrapInClass(@"protected a, b, c as usual");
+            var expected = WrapInClass(@"protected a as usual, b as string, c as usual");
+            var parser = code.ParseText();
+            var vars = GetVars(parser);
+
+            var toBeReplacedVar = vars.Single(x => x.Name.Equals("b")).Context;
+
+            parser.Tree.RewriterFor(toBeReplacedVar).ReplaceType("string");
+            AssertRewriterTextEquals(parser, expected);
+        }
+
+        private static ClassvarContextValues[] GetVars(ParserHelper parser)
+        {
+            return parser.Tree.WhereType<ClassvarsContext>()
+                            .Single()
+                            .ToValues().Vars;
+        }
+
+        private void AssertRewriterTextEquals(ParserHelper parser, string expected)
+        {
+            var resultLines = parser.Tree.Rewriter.GetText().SplitLines();
+            var expectedLines = expected.SplitLines();
+            for (var i = 0; i < resultLines.Length; i++)
+                resultLines[i].Should().Be(expectedLines.ElementAtOrDefault(i), $"Line {i}");
+        }
+    }
+}
---
+++ src/XSharp.VsParser.Helpers/Rewriter/RewriterForClassvarContextExtensions.cs
@@ -0,0 +1,60 @@
+﻿using LanguageService.SyntaxTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using XSharp.VsParser.Helpers.Parser;
+using static LanguageService.CodeAnalysis.XSharp.SyntaxParser.XSharpParser;
+
+namespace XSharp.VsParser.Helpers.Rewriter
+{
+    /// <summary>
+    /// RewriterForPropertyClassVar Extensions
+    /// </summary>
+    public static class RewriterForClassvarContextExtensions
+    {
+
+        /// <summary>
+        /// Replaces the type of the class var
+        /// </summary>
+        /// <param name="rewriterFor">The rewriterFor instance</param>
+        /// <param name="newType">The new type</param>
+        /// <returns>The rewriterFor instance</returns>
+        public static RewriterForContext<ClassvarContext> ReplaceType(this RewriterForContext<ClassvarContext> rewriterFor, string newType)
+        {
+            if (string.IsNullOrEmpty(newType))
+                throw new ArgumentException($"{nameof(newType)} can not be empty");
+
+            var context = rewriterFor.Context;
+            var typeContext = context.DataType;
+            if (typeContext != null && HasTypeDefinition(context))
+            {
+                rewriterFor.Rewriter.Replace(typeContext.start.ToIndex(), typeContext.stop.ToIndex(), InternalRewriterHelper.RemoveAsFromType(newType));
+            }
+            else
+                rewriterFor.Rewriter.InsertAfter(context.stop.ToIndex(), " " + InternalRewriterHelper.AddAsToType(newType));
+
+            MoveTypeDefinition(rewriterFor, context);
+
+            return rewriterFor;
+        }
+
+        private static void MoveTypeDefinition(RewriterForContext<ClassvarContext> rewriterFor, ClassvarContext variable)
+        {
+            var prevVariable = GetPreviousVariable(variable);
+            if (prevVariable != null && !HasTypeDefinition(prevVariable))
+            {
+                rewriterFor.Rewriter.InsertAfter(prevVariable.stop.ToIndex(), " " + InternalRewriterHelper.AddAsToType(variable.DataType.GetText()));
+            }
+        }
+        private static bool HasTypeDefinition(ClassvarContext classvar) => classvar.As != null;
+
+        private static ClassvarContext GetPreviousVariable(ClassvarContext context)
+        {
+            var parent = context.Parent as ClassvarsContext;
+
+            return parent.children.WhereType<ClassvarContext>()
+                .TakeWhile(x => x != context)
+                .LastOrDefault();
+        }
+    }
+}
