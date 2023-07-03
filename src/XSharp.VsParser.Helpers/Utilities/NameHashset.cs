cd898c9ce8caae1f2e244ee58b133253b85845ba
Volkmar Rigo <Volkmar.rigo@Infominds.eu>
2023-07-03 14:21:11 +0200

    Added NameHashset and MappedDictionary

---
+++ src/XSharp.VsParser.Helpers/Utilities/NameHashset.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XSharp.VsParser.Helpers.Utilities
+{
+    /// <summary>
+    /// A Hashset Class for case-insensitive names
+    /// </summary>
+    public class NameHashset : HashSet<string>
+    {
+        /// <summary>
+        /// Constructur
+        /// </summary>
+        public NameHashset() : base(StringComparer.OrdinalIgnoreCase)
+        {
+
+        }
+
+        /// <summary>
+        /// Constuctor
+        /// </summary>
+        /// <param name="items"></param>
+        public NameHashset(IEnumerable<string> items) : this()
+        {
+            foreach (string item in items ?? Enumerable.Empty<string>())
+                Add(item);
+        }
+    }
+}
