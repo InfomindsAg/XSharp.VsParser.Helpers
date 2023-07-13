d86c693a7d9ebd55a814b2cd97836462cebc2077
Volkmar Rigo <Volkmar.rigo@Infominds.eu>
2023-07-13 10:21:44 +0200

    Added benchmark project

---
+++ src/XSharp.VsParser.Helper.Benchmarks/Config.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Reports;
+
+class Config : ManualConfig
+{
+
+    public Config()
+    {
+        AddLogger(DefaultConfig.Instance.GetLoggers().ToArray());
+        AddColumnProvider(DefaultConfig.Instance.GetColumnProviders().ToArray());
+        AddExporter(new CsvExporter(
+            CsvSeparator.CurrentCulture,
+            new SummaryStyle(
+                System.Globalization.CultureInfo.CurrentCulture,
+                true, //printUnitsInHeader
+                SizeUnit.KB,
+                Perfolizer.Horology.TimeUnit.Microsecond,
+                false //printUnitsInContent
+            )
+        ));
+    }
+}
---
+++ src/XSharp.VsParser.Helper.Benchmarks/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using BenchmarkDotNet.Exporters.Csv;
+
+var config = new Config()
+      .WithOptions(ConfigOptions.JoinSummary)
+      .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+      .AddDiagnoser(MemoryDiagnoser.Default);
+
+
+BenchmarkRunner.Run(typeof(HashBenchmarks), config);
+
+// new HashBenchmarks().ConsoleOutput();
+// Console.ReadLine();
+
